5628e6fa2454e1daca2e7944ca838f095fc18e1e
chiuleung <chiu-leung@Hotmail.com>
2018-08-11 12:50:18 -0400

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: true, 
   firebase: {
     apiKey: "AIzaSyA2169ZVSLzev1d_VAXYZYNrFHLSaLWAoc",
     authDomain: "angularfire-a3c5d.firebaseapp.com",
